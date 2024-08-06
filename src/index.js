ce92a57924967e310a6b57ee781823319c56fb31
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-08-06 10:46:02 +0200

    server up public directory and render html file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    console.log(`Server is up on port ${port}!`)
 })
