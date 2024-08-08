3154b9d5ae6460c81224bf4004246dfa2ec74067
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-08-08 11:20:58 +0200

    refactore  chat.js file to apply state for form elements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,31 @@
 const socket = io()
 
+//elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+
+    socket.emit('sendMessage', message, () => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        console.log('Message delivered!')
+
+
+    })
 })
 
 
