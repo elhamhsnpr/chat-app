fe745e016030d8efa30dc11f08fa20431398ffa8
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-08-08 11:32:28 +0200

    refactore sendlocation button in chat fileand add state for it

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,7 @@ const socket = io()
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
 
 socket.on('message', (message) => {
     console.log(message)
@@ -29,18 +30,22 @@ $messageForm.addEventListener('submit', (e) => {
 })
 
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
 
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+
             console.log('Location shared!')
+            $sendLocationButton.removeAttribute('disabled')
         })
     })
 })
