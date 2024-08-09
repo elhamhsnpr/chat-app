4744361894d6b6c098623079f3b76ca46e8ef7f4
Elham Hasanpour <elhamhassanpour133@gmail.com>
2024-08-09 08:02:15 +0000

    render message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,22 @@
 const socket = io()
 
-//elements
+//Elements
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 
