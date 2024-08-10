6255ce96b4b47f868a36e00050b81a813accfef1
elhamhsnpr <elhamhassanpour133@gmail.com>
2024-08-10 17:24:21 +0200

    add getUser and getUsersInRoom function and exports them

--- src/utils/users.js
+++ src/utils/users.js
@@ -10,9 +10,9 @@ const addUser = ({ id, username, room }) => {
     };
   }
 
-  const existingUser = users.find((user) => {
-    return user.username === usern && user.room === room;
-  });
+  const existingUser = users.find(
+    (user) => user.username === username && user.room === room
+  );
 
   if (existingUser) {
     return {
@@ -30,4 +30,20 @@ const removeUser = (id) => {
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
+};
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase();
+  return users.filter((user) => user.room == room);
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
 };
